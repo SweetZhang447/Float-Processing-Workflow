65430440894f424abb677b1fb4505b35e3b00c32
Sweet Zhang <80295133+SweetZhang447@users.noreply.github.com>
2026-07-30 19:48:58 -0700

    sdf

--- modules/phy_parser.py
+++ modules/phy_parser.py
@@ -479,8 +479,7 @@ def _generate_phy_file(
 
     sorted_files = sorted(file_list, key=_sort_key)
 
-    print("print")
-
+    print("hi")
     for v in sorted_files:
         if 'meta' in v:
             _getMETAInfo(meta_file, float_dict_info)
